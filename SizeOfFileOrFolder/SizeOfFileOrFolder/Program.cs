c29763054bc9f2fdf8972b3d602f5b1a97ff4c69
Silvia Papara <56363962+SilviaPapara@users.noreply.github.com>
2020-04-18 14:49:06 +0300

    Add files via upload

---
+++ SizeOfFileOrFolder/SizeOfFileOrFolder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SizeOfFileOrFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo info = new DirectoryInfo(@"D:/Poze"); // folderul dorit
+            long MarDir = SOD(info, true);
+            Console.WriteLine("Marimea folderului este : " + "{0:N0} Bytes", MarDir);
+
+
+            Console.ReadLine();
+           
+        }
+
+
+        static long SOD(DirectoryInfo info, bool b)
+        {
+            long MarTot = info.EnumerateFiles()
+                         .Sum(file => file.Length);
+            if (b)
+            {
+                MarTot += info.EnumerateDirectories()
+                         .Sum(dir => SOD(dir, true));
+            }
+            return MarTot;
+        }
+    
+    }
+
+}
